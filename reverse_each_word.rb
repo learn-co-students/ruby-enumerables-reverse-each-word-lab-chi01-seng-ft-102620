a52294a8e4efdc9249b618ab53f8148cd58dcd6f
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-21 01:34:07 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(string)
   new_arr = []
-  new_arr = string.split(" ").each do |word| word.reverse!
-  end
-  new_arr.join(" ")
+  new_arr = string.split(" ").each {|word| word.reverse}.join(" ")
+  new_arr
   string.split(" ").collect {|word| word.reverse}.join(" ")
 end
-
-
